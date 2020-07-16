f686639aa9f5e15f6871c996e8b601e1cfbe001d
samuelzandesk01 <samuelseniga18@gmail.com>
2020-07-16 11:33:14 +0100

    Updates

---
+++ precache-manifest.7e2b09124e4058c2bc94565130a3544a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c86a6accb0088afb0e546e1a6ba89a1",
+    "url": "/project-idea/index.html"
+  },
+  {
+    "revision": "8947c6b72656e43a8fc3",
+    "url": "/project-idea/static/css/main.657b0762.chunk.css"
+  },
+  {
+    "revision": "7efd1bbef2414fbb3bb7",
+    "url": "/project-idea/static/js/2.4f8697b1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/project-idea/static/js/2.4f8697b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8947c6b72656e43a8fc3",
+    "url": "/project-idea/static/js/main.9f172b9f.chunk.js"
+  },
+  {
+    "revision": "a49b3510f6d889bbaab2",
+    "url": "/project-idea/static/js/runtime-main.2c022f23.js"
+  },
+  {
+    "revision": "d2a92c73f2f87bd5ece91fdbef079822",
+    "url": "/project-idea/static/media/app-3.d2a92c73.png"
+  },
+  {
+    "revision": "2f5c3fd7dbbee502ddfd6e1d82856cba",
+    "url": "/project-idea/static/media/app.2f5c3fd7.png"
+  },
+  {
+    "revision": "0587f70c98d05d35833d892558712ade",
+    "url": "/project-idea/static/media/facebook.0587f70c.svg"
+  },
+  {
+    "revision": "df7263045f06484ab7470dfd84e86004",
+    "url": "/project-idea/static/media/google.df726304.png"
+  },
+  {
+    "revision": "aa58bf115dfe5a6b0d8f95c798d9cdfe",
+    "url": "/project-idea/static/media/illustration-2.aa58bf11.svg"
+  },
+  {
+    "revision": "c0fa32943e9bbc49a9cf7d5c3c1ae52b",
+    "url": "/project-idea/static/media/illustration-8.c0fa3294.png"
+  },
+  {
+    "revision": "6f7a9833e0c7899a1d73415ccdbdf3da",
+    "url": "/project-idea/static/media/presentation-1.6f7a9833.png"
+  },
+  {
+    "revision": "640567cf49e2a3c17bdb6a9bdefbc0be",
+    "url": "/project-idea/static/media/printest.640567cf.png"
+  },
+  {
+    "revision": "58d6dc1c7be1cfca5f8a5c24eb34f749",
+    "url": "/project-idea/static/media/product-1.58d6dc1c.png"
+  },
+  {
+    "revision": "667023fb2884d079a414e2c1b014fd66",
+    "url": "/project-idea/static/media/product-2.667023fb.png"
+  },
+  {
+    "revision": "c1429ccb5eaedf136ab68e5f5fe8a46a",
+    "url": "/project-idea/static/media/product-3.c1429ccb.png"
+  },
+  {
+    "revision": "c51a383cd0593573a571191b157057da",
+    "url": "/project-idea/static/media/product-4.c51a383c.png"
+  },
+  {
+    "revision": "e060b0fd5459dc98f221fca06c44417a",
+    "url": "/project-idea/static/media/product-5.e060b0fd.png"
+  },
+  {
+    "revision": "9e506259e6350436a4831f26ed803ca7",
+    "url": "/project-idea/static/media/product-6.9e506259.png"
+  },
+  {
+    "revision": "7901ad7cc5b7f75f886182e4c035d16b",
+    "url": "/project-idea/static/media/trust.7901ad7c.png"
+  },
+  {
+    "revision": "5c95677dd032145d0815081710e5544c",
+    "url": "/project-idea/static/media/undraw_coding_6mjf.5c95677d.png"
+  },
+  {
+    "revision": "7787536334447a7adfa6b89746cbe430",
+    "url": "/project-idea/static/media/undraw_dev_productivity_umsq.77875363.svg"
+  },
+  {
+    "revision": "6137abf6e4fe739913b94a09363847d4",
+    "url": "/project-idea/static/media/undraw_developer_activity_bv83.6137abf6.svg"
+  }
+]);
